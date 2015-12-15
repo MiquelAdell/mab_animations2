bebbb3658703a537b19f9d7f2f4861a86c4c932d
Miquel Adell Borràs <miquel.adell@gmail.com>
2015-12-15 13:44:56 +0100

    first commit

---
+++ functions.js
@@ -0,0 +1,14 @@
+$(function(){
+    var setTime = function(){
+        var date = new Date(),
+        MINUTE = 60, HOUR   = 60*MINUTE,
+        seconds = date.getSeconds(),
+        minutes = (date.getMinutes()*MINUTE) + seconds,
+        hours = (date.getHours()*HOUR)+minutes;
+        document.getElementById('second-hand').setAttribute('transform', 'rotate('+360*(seconds/MINUTE)+',16,17)');
+        document.getElementById('minute-hand').setAttribute('transform', 'rotate('+360*(minutes/HOUR)+',16,17)');
+        document.getElementById('hour-hand').setAttribute('transform', 'rotate('+360*(hours/(12*HOUR))+',16,17)');
+    }
+    setTime();
+    var interval = setInterval(setTime,1000);
+});
